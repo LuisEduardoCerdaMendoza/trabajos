64446cf3b224a0dd9d07f3c2f800e035817a6599
LuisEduardoCerdaMendoza <82552346+LuisEduardoCerdaMendoza@users.noreply.github.com>
2021-06-23 11:01:09 -0500

    Update SumasDeRiemannLuisEduardoCerdaMendoza3201.m

--- SumasDeRiemannLuisEduardoCerdaMendoza3201.m
+++ SumasDeRiemannLuisEduardoCerdaMendoza3201.m
@@ -1,1 +1,28 @@
-# Created by Octave 6.2.0, Wed Jun 23 11:00:09 2021 GMT <unknown@DESKTOP-2I7DIP5>
+% Sumas De Riemann
+% Título: Sumas Inferiores y Superiores.
+% Descripción: Script para saber cualquier suma de riemann
+% Autor: Luis Eeduardo Cerda Mendoza (LuisEduardoCerdaMendoza) luis270502@outlook.com  
+% Fecha: 20210622
+% Sesion: 1
+% Notas: Requiere aplicacion octave usar en consola preferiblemente
+
+%Limpiamos_variables
+  clear;
+  a = -3; b = 3;
+  n_rec = 500;
+  delta_x = (b-a)/n_rec;
+  x=zeros(1,n_rec+1);
+  for i = 1:length(x)
+    x(i)=abs (a + (i-1)*delta_x);
+  endfor
+
+%xx=linspace(a,b,n_rec+1);
+f =feval('funcion',x);
+Areat=0;
+for i = 1: n_rec
+  Areat =abs (Areat + f(x(i+1))*delta_x);
+endfor
+Area=[Areat];
+double (Area)
+
+%Nota: Utilizar el archivo llamado funcion
