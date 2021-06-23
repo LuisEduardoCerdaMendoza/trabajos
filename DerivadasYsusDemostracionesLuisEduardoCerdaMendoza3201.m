b570a4377a3eaeb0e85d7e829e1f1c7869ac29d1
LuisEduardoCerdaMendoza <82552346+LuisEduardoCerdaMendoza@users.noreply.github.com>
2021-06-23 10:51:34 -0500

    Update DerivadasYsusDemostracionesLuisEduardoCerdaMendoza3201.m

--- DerivadasYsusDemostracionesLuisEduardoCerdaMendoza3201.m
+++ DerivadasYsusDemostracionesLuisEduardoCerdaMendoza3201.m
@@ -1,1 +1,21 @@
-# Created by Octave 6.2.0, Wed Jun 23 10:50:12 2021 GMT <unknown@DESKTOP-2I7DIP5>
+% Título: Derivadas y sus demostraciones
+% Descripción: Script para graficar derivadas.
+% Autor: Luis Eeduardo Cerda Mendoza (LuisEduardoCerdaMendoza) luis270502@outlook.com 
+% Fecha: 2021-06-23
+% Sesion: 1
+% Notas: Requiere aplicacion octave... usar en consola preferiblemente
+
+%Limpiamos Variables
+clear
+
+%Utilizaremos syms para el valor de x
+syms x;
+
+%Definimos la función f y g
+f=(x^2);
+g= ( 4 * x);
+
+%Para las derivadas se utiliza diff
+Suma = diff(f) + diff(g)
+Multiplicacion = diff(x^2 * 4 * x)
+Division = diff( f / g)
